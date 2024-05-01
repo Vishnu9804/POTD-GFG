23dfcf1e5443d45691ccc6cdd7d207718de91f32
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-01 20:05:19 +0530

    Add files via upload

---
+++ POTD_30_4_2024.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node *buildList(int size)
+{
+    int val;
+    cin >> val;
+
+    Node *head = new Node(val);
+    Node *tail = head;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        cin >> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+
+    return head;
+}
+
+void printList(Node *n)
+{
+    while (n)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
