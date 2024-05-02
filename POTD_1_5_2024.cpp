327a80a23fd1885639fb2f2dd5d8cb1ef9239cf9
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-02 14:22:45 +0530

    Update POTD_1_5_2024.cpp

--- POTD_1_5_2024.cpp
+++ POTD_1_5_2024.cpp
@@ -43,3 +43,67 @@ void append(struct Node **headRef, char data)
     last->next = new_node;
     return;
 }
+class Solution
+{
+public:
+    void deleteVal(Node *&head, int val)
+    {
+        if (head == nullptr)
+            return;
+
+        if (head->data == val)
+        {
+            Node *temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+
+        Node *prevNode = head;
+        Node *temp = head->next;
+
+        while (temp != nullptr && temp->data != val)
+        {
+            prevNode = temp;
+            temp = temp->next;
+        }
+
+        if (temp == nullptr)
+            return;
+
+        prevNode->next = temp->next;
+        delete temp;
+    }
+    void insertAtFirst(Node *&head, int val)
+    {
+        Node *n = new Node(val);
+        n->next = head;
+        head = n;
+    }
+
+    struct Node *arrangeCV(Node *head)
+    {
+        Node *temp = head;
+        stack<char> st;
+        while (temp)
+        {
+            if (temp->data == 'a' || temp->data == 'e' || temp->data == 'i' || temp->data == 'o' || temp->data == 'u')
+            {
+                st.push(temp->data);
+                Node *tempDelete = temp;
+                temp = temp->next;
+                deleteVal(head, tempDelete->data);
+            }
+            else
+            {
+                temp = temp->next;
+            }
+        }
+        while (!st.empty())
+        {
+            insertAtFirst(head, st.top());
+            st.pop();
+        }
+        return head;
+    }
+};
