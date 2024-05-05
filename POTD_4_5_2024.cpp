e1261ae0ce2181a2a468ad8fe6c6e2010e774bd0
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-05 09:29:35 +0530

    Update POTD_4_5_2024.cpp

--- POTD_4_5_2024.cpp
+++ POTD_4_5_2024.cpp
@@ -34,3 +34,41 @@ void preOrder(Node *node)
     /* now recur on right child */
     preOrder(node->right);
 }
+
+class Solution
+{
+public:
+    Node *buildTreeUtil(int in[], int post[], int inStrt, int inEnd, int *index)
+    {
+        if (inStrt > inEnd)
+            return NULL;
+
+        Node *tNode = new Node(post[*index]);
+        (*index)--;
+
+        if (inStrt == inEnd)
+            return tNode;
+
+        int inIndex = search(in, inStrt, inEnd, tNode->data);
+        tNode->right = buildTreeUtil(in, post, inIndex + 1, inEnd, index);
+        tNode->left = buildTreeUtil(in, post, inStrt, inIndex - 1, index);
+
+        return tNode;
+    }
+
+    int search(int arr[], int strt, int end, int value)
+    {
+        int i;
+        for (i = strt; i <= end; i++)
+        {
+            if (arr[i] == value)
+                return i;
+        }
+    }
+
+    Node *buildTree(int in[], int post[], int n)
+    {
+        int index = n - 1;
+        return buildTreeUtil(in, post, 0, n - 1, &index);
+    }
+};
