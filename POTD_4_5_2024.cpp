0c21f8d07167ee1a7b6041768ba93641c35e5dfb
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-05 09:28:28 +0530

    Add files via upload

---
+++ POTD_4_5_2024.cpp
@@ -0,0 +1,36 @@
+#include <cstddef>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node *node)
+{
+    if (node == NULL)
+        return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
