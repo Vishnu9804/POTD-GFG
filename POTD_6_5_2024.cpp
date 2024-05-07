7595e5e510978926ce302bd929453c5471ce951e
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-07 13:19:53 +0530

    Update POTD_6_5_2024.cpp

--- POTD_6_5_2024.cpp
+++ POTD_6_5_2024.cpp
@@ -1,15 +1,7 @@
 // POTD geeksforgeeks 06/05/2024
 // Print all nodes that don't have sibling
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <sstream>
-#include <queue>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
 struct Node
 {
