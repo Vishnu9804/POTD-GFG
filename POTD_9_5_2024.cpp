4e1a3cdc0338a4748e5aff93d1e0aa0a6d93ec74
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-10 00:37:16 +0530

    Add files via upload

---
+++ POTD_9_5_2024.cpp
@@ -0,0 +1,15 @@
+// POTD geeksforgeeks 09/05/2024
+// Devisor Game
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    bool divisorGame(int n)
+    {
+        return n % 2 == 0;
+    }
+};
