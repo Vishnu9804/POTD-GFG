2133332196b3a36d81582c33d0841c440cc809dd
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-13 12:13:45 +0530

    Add files via upload

---
+++ POTD_10_5_2024.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> CombinationSum2(vector<int> arr, int n, int k)
+    {
+        vector<vector<int>> result;
+        vector<int> current;
+        sort(arr.begin(), arr.end());
+        backtrack(arr, k, 0, current, result);
+        return result;
+    }
+
+    void backtrack(vector<int> &arr, int target, int start, vector<int> &current, vector<vector<int>> &result)
+    {
+        if (target == 0)
+        {
+            result.push_back(current);
+            return;
+        }
+        if (target < 0)
+            return;
+
+        for (int i = start; i < arr.size(); ++i)
+        {
+            if (i > start && arr[i] == arr[i - 1])
+                continue;
+            current.push_back(arr[i]);
+            backtrack(arr, target - arr[i], i + 1, current, result);
+            current.pop_back();
+        }
+    }
+};
