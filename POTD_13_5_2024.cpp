18c721b4bd0f48b16508878baed9970ed16b386b
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-16 12:05:24 +0530

    Add files via upload

---
+++ POTD_13_5_2024.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Matrix
+{
+public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            for (int j = 0; j < A[i].size(); j++)
+            {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
