701af04d76e36ad0127d3cd7629c0ca0ef81357d
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-18 12:37:33 +0530

    Update POTD_15_5_2024.cpp

--- POTD_15_5_2024.cpp
+++ POTD_15_5_2024.cpp
@@ -54,3 +54,53 @@ public:
         }
     }
 };
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<vector<string>> accounts;
+
+        for (int i = 0; i < n; i++)
+        {
+            vector<string> temp;
+            int x;
+            cin >> x;
+
+            for (int j = 0; j < x; j++)
+            {
+                string s1;
+                cin >> s1;
+                temp.push_back(s1);
+            }
+            accounts.push_back(temp);
+        }
+
+        Solution obj;
+        vector<vector<string>> res = obj.accountsMerge(accounts);
+        sort(res.begin(), res.end());
+        cout << "[";
+        for (int i = 0; i < res.size(); ++i)
+        {
+            cout << "[";
+            for (int j = 0; j < res[i].size(); j++)
+            {
+                if (j != res[i].size() - 1)
+                    cout << res[i][j] << ","
+                         << " ";
+                else
+                    cout << res[i][j];
+            }
+            if (i != res.size() - 1)
+                cout << "], " << endl;
+            else
+                cout << "]";
+        }
+        cout << "]"
+             << endl;
+    }
+}
