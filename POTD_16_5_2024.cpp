707ab0b302e0295c3fe26d17ce8729199e660bf0
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-19 15:54:00 +0530

    Add files via upload

---
+++ POTD_16_5_2024.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int dfs(int node, int parent, vector<vector<int>> &adj, vector<int> &subtreeSize, int &removableEdges)
+    {
+        int size = 1;
+        for (int neighbor : adj[node])
+        {
+            if (neighbor != parent)
+            {
+                size += dfs(neighbor, node, adj, subtreeSize, removableEdges);
+            }
+        }
+        subtreeSize[node] = size;
+        if (parent != -1 && size % 2 == 0)
+        {
+            removableEdges++;
+        }
+
+        return size;
+    }
+
+    int minimumEdgeRemove(int n, vector<vector<int>> &edges)
+    {
+        vector<vector<int>> adj(n + 1);
+        for (const auto &edge : edges)
+        {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        vector<int> subtreeSize(n + 1, 0);
+        int removableEdges = 0;
+
+        dfs(1, -1, adj, subtreeSize, removableEdges);
+
+        return removableEdges;
+    }
+};
