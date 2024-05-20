028c67e2c10d569a870d27bf35bc82e3268c9c55
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-20 12:02:41 +0530

    Update POTD_17_5_2024.cpp

--- POTD_17_5_2024.cpp
+++ POTD_17_5_2024.cpp
@@ -29,6 +29,31 @@ class Solution
 public:
     int findPair(int n, int x, vector<int> &arr)
     {
+        sort(arr.begin(), arr.end());
+
+        int left = 0;
+        int right = 1;
+
+        while (right < n)
+        {
+            int diff = abs(arr[right] - arr[left]);
+            if (diff == x)
+            {
+                return 1;
+            }
+            else if (diff < x)
+            {
+                right++;
+            }
+            else
+            {
+                left++;
+                if (left == right)
+                    right++;
+            }
+        }
+
+        return -1;
     }
 };
 
