475f4cfdad51973206b27b656fef73990894fcda
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-24 11:51:03 +0530

    Update POTD_21_5_2024.cpp

--- POTD_21_5_2024.cpp
+++ POTD_21_5_2024.cpp
@@ -6,6 +6,76 @@ class Solution
 public:
     vector<int> printKClosest(vector<int> numbers, int size, int kValue, int target)
     {
+        vector<int> result;
+        vector<int> closestValues;
+
+        int left = findCrossOver(numbers, 0, size - 1, target);
+
+        int right = left + 1;
+        int counter = 0;
+
+        if (numbers[left] == target)
+        {
+            left--;
+        }
+
+        while (left >= 0 && right < size && counter < kValue)
+        {
+            if (target - numbers[left] < numbers[right] - target)
+            {
+                closestValues.push_back(numbers[left]);
+                left--;
+            }
+            else
+            {
+                closestValues.push_back(numbers[right]);
+                right++;
+            }
+            counter++;
+        }
+
+        while (counter < kValue && left >= 0)
+        {
+            closestValues.push_back(numbers[left]);
+            left--;
+            counter++;
+        }
+
+        while (counter < kValue && right < size)
+        {
+            closestValues.push_back(numbers[right]);
+            right++;
+            counter++;
+        }
+
+        for (int i = 0; i < kValue; i++)
+        {
+            result.push_back(closestValues[i]);
+        }
+
+        return result;
+    }
+
+    int findCrossOver(vector<int> &values, int lower, int higher, int target)
+    {
+        if (values[higher] <= target)
+        {
+            return higher;
+        }
+        if (values[lower] > target)
+        {
+            return lower;
+        }
+        int mid = (lower + higher) / 2;
+        if (values[mid] <= target && values[mid + 1] > target)
+        {
+            return mid;
+        }
+        else if (values[mid] < target)
+        {
+            return findCrossOver(values, mid + 1, higher, target);
+        }
+        return findCrossOver(values, lower, mid - 1, target);
     }
 };
 
