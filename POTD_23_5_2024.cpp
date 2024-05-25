06e2c5ac7b85790381b52cd8d1ea856c5d6478ad
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-26 01:01:34 +0530

    Update POTD_23_5_2024.cpp

--- POTD_23_5_2024.cpp
+++ POTD_23_5_2024.cpp
@@ -6,6 +6,25 @@ class Solution
 public:
     int kPalindrome(string str, int n, int k)
     {
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int length = 2; length <= n; ++length)
+        {
+            for (int i = 0; i <= n - length; ++i)
+            {
+                int j = i + length - 1;
+                if (str[i] == str[j])
+                {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1] <= k ? 1 : 0;
     }
 };
 
