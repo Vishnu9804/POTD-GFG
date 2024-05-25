63b30aa801a21cf2393bdcaba6089cae4bcb6e5e
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-26 00:59:56 +0530

    Add files via upload

---
+++ POTD_23_5_2024.cpp
@@ -0,0 +1,30 @@
+// POTD geeksforgeeks 23/05/2024
+// K-Palindrome
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int kPalindrome(string str, int n, int k)
+    {
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string str;
+        cin >> str;
+
+        Solution ob;
+        cout << ob.kPalindrome(str, n, k) << endl;
+    }
+    return 0;
+}
