39a8a18529b74f592e0bf4249204032a6de723ab
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-27 13:11:12 +0530

    Update POTD_24_5_2024.cpp

--- POTD_24_5_2024.cpp
+++ POTD_24_5_2024.cpp
@@ -9,6 +9,28 @@ class Solution
 public:
     int countPartitions(int n, int d, vector<int> &arr)
     {
+        const int MOD = 1e9 + 7;
+        int sum = accumulate(arr.begin(), arr.end(), 0);
+
+        if ((d + sum) % 2 != 0)
+            return 0;
+
+        int target_sum = (d + sum) / 2;
+        if (target_sum < 0)
+            return 0;
+
+        vector<int> dp(target_sum + 1, 0);
+        dp[0] = 1;
+
+        for (int num : arr)
+        {
+            for (int j = target_sum; j >= num; --j)
+            {
+                dp[j] = (dp[j] + dp[j - num]) % MOD;
+            }
+        }
+
+        return dp[target_sum];
     }
 };
 
