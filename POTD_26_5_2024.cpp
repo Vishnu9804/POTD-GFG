c2ac03a194070e73bf7267de049c71c298cfb7a7
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-29 01:00:00 +0530

    Update POTD_26_5_2024.cpp

--- POTD_26_5_2024.cpp
+++ POTD_26_5_2024.cpp
@@ -1,17 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
 class Solution
 {
 
 public:
     int findMinCost(string x, string y, int costX, int costY)
     {
+        int m = x.length();
+        int n = y.length();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= m; ++i)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
+                if (x[i - 1] == y[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int lcsLength = dp[m][n];
+
+        int cost = (m - lcsLength) * costX + (n - lcsLength) * costY;
+
+        return cost;
     }
 };
 
-//{ Driver Code Starts.
 int main()
 {
 
