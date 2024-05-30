fd62291e2a0291bee5bb89d313b75fe3ea990fcf
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-30 22:17:44 +0530

    Add files via upload

---
+++ POTD_27_5_2024.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Solution
+{
+public:
+    int longestSubseq(int n, vector<int> &a)
+    {
+    }
+};
+
+int main()
+{
+    int t;
+    scanf("%d ", &t);
+    while (t--)
+    {
+
+        int n;
+        scanf("%d", &n);
+
+        vector<int> a(n);
+        Array::input(a, n);
+
+        Solution obj;
+        int res = obj.longestSubseq(n, a);
+
+        cout << res << endl;
+    }
+}
