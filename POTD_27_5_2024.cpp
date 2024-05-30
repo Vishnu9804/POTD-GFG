ec9be2305588f2c2e41c530f3b76bfbb76594715
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-30 22:18:53 +0530

    Update POTD_27_5_2024.cpp

--- POTD_27_5_2024.cpp
+++ POTD_27_5_2024.cpp
@@ -29,6 +29,20 @@ class Solution
 public:
     int longestSubseq(int n, vector<int> &a)
     {
+        vector<int> dp(n, 1);
+
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (abs(a[i] - a[j]) == 1)
+                {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
     }
 };
 
