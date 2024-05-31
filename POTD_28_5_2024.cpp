e6381275e8ffe7aac5ae712d25e8b17c19c556a7
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-05-31 10:54:20 +0530

    Add files via upload

---
+++ POTD_28_5_2024.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Solution
+{
+public:
+    int minimumCost(int n, int w, vector<int> &cost)
+    {
+    }
+};
+
+int main()
+{
+    int t;
+    scanf("%d ", &t);
+    while (t--)
+    {
+
+        int n;
+        scanf("%d", &n);
+
+        int w;
+        scanf("%d", &w);
+
+        vector<int> cost(n);
+        Array::input(cost, n);
+
+        Solution obj;
+        int res = obj.minimumCost(n, w, cost);
+
+        cout << res << endl;
+    }
+}
