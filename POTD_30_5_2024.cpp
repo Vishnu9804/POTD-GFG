ac050bfa325940ff8306508cc57a5195546b67ed
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-06-02 11:33:03 +0530

    Update POTD_30_5_2024.cpp

--- POTD_30_5_2024.cpp
+++ POTD_30_5_2024.cpp
@@ -6,6 +6,22 @@ class Solution
 public:
     int countWays(string s1, string s2)
     {
+        int n = s1.length(), m = s2.length();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i <= n; ++i)
+            dp[i][0] = 1;
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= m; ++j)
+            {
+                dp[i][j] = dp[i - 1][j];
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % 1000000007;
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
 
