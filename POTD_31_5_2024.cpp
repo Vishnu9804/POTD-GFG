9bdfcbe6c30fd411a66afdc087e71fb385f32c63
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-06-03 13:26:18 +0530

    Add files via upload

---
+++ POTD_31_5_2024.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int swapNibbles(int n)
+    {
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.swapNibbles(n) << endl;
+    }
+    return 0;
+}
