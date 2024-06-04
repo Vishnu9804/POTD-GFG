783859bea04056de9320e8fbfcd9ab517114cf39
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-06-04 12:09:13 +0530

    Update POTD_1_6_2024.cpp

--- POTD_1_6_2024.cpp
+++ POTD_1_6_2024.cpp
@@ -6,6 +6,45 @@ class Solution
 public:
     string oddEven(string s)
     {
+        vector<int> freq(26, 0);
+        for (char c : s)
+        {
+            freq[c - 'a']++;
+        }
+
+        int x = 0, y = 0;
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (freq[i] > 0)
+            {
+                int pos = i + 1;
+                if (pos % 2 == 0)
+                {
+                    if (freq[i] % 2 == 0)
+                    {
+                        x++;
+                    }
+                }
+                else
+                {
+                    if (freq[i] % 2 == 1)
+                    {
+                        y++;
+                    }
+                }
+            }
+        }
+
+        int sum = x + y;
+        if (sum % 2 == 0)
+        {
+            return "EVEN";
+        }
+        else
+        {
+            return "ODD";
+        }
     }
 };
 
