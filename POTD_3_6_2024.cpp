79e2c996c75f90545ab5148047c8bab5896df1c7
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-06-07 21:54:39 +0530

    Add files via upload

---
+++ POTD_3_6_2024.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int numberOfConsecutiveOnes(int n)
+    {
+    }
+};
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.numberOfConsecutiveOnes(N) << endl;
+    }
+    return 0;
+}
