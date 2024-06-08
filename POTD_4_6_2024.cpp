d7838fbeebc2dd8c396464d1855908085e3a42ea
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-06-08 15:45:27 +0530

    Add files via upload

---
+++ POTD_4_6_2024.cpp
@@ -0,0 +1,57 @@
+// POTD geeksforgeeks 04/06/2024
+// Binary representation of next number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string binaryNextNumber(string s)
+    {
+
+        int i, l = s.size();
+
+        string ans = "";
+        for (i = l - 1; i >= 0; i--)
+        {
+            if (s[i] == '1')
+                s[i] = '0';
+            else
+            {
+                s[i] = '1';
+                break;
+            }
+        }
+        if (i < 0)
+        {
+            ans += '1';
+            i = 0;
+        }
+        else
+        {
+            i = 0;
+            while (s[i] == '0')
+                i++;
+        }
+        ans += s.substr(i, l);
+
+        return ans;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.binaryNextNumber(s);
+        cout << "\n";
+    }
+
+    return 0;
+}
