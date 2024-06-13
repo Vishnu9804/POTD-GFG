5cfb341ba646a3967994d3193f21f44ccd4f08cb
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-06-13 11:41:14 +0530

    Create POTD_13_6_2024.cpp

---
+++ POTD_13_6_2024.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int padovanSequence(int n)
+    {
+        const int MOD = 1000000007;
+        if (n == 0 || n == 1 || n == 2)
+        {
+            return 1;
+        }
+
+        vector<long long> P(n + 1, 0);
+        P[0] = 1;
+        P[1] = 1;
+        P[2] = 1;
+
+        for (int i = 3; i <= n; ++i)
+        {
+            P[i] = (P[i - 2] + P[i - 3]) % MOD;
+        }
+
+        return P[n];
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.padovanSequence(n) << endl;
+    }
+    return 0;
+}
