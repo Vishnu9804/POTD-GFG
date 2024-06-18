37026c19a25cba0f85d196aded2ad9a34816f0f2
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-06-19 00:49:49 +0530

    Create POTD_18_06_2024.cpp

---
+++ POTD_18_06_2024.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int rectanglesInCircle(int r)
+    {
+        int count = 0;
+        int limit = 2 * r;
+        for (int a = 1; a <= limit; ++a)
+        {
+            for (int b = 1; b <= limit; ++b)
+            {
+                if (a * a + b * b <= 4 * r * r)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        int ans = ob.rectanglesInCircle(n);
+        cout << ans << "\n";
+    }
+}
