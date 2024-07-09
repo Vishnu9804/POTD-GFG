2aef521a942f6979710f13677b08179d7c53ce8a
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-07-09 21:35:30 +0530

    Update

---
+++ POTD_8_7_2024.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        int left = 0;
+        int right = arr.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (arr[mid] == key) {
+                return mid;
+            }
+            
+            // Check if left half is sorted
+            if (arr[left] <= arr[mid]) {
+                // Key lies in the left sorted half
+                if (key >= arr[left] && key < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // Right half is sorted
+                if (key > arr[mid] && key <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        
+        // Key not found
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(arr, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
