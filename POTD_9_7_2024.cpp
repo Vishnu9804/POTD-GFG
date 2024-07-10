5551ba980b29e9f198e535fe17fbb2fbeb4ea67c
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-07-10 21:26:18 +0530

    Update

---
+++ POTD_9_7_2024.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+// arr : given vector of elements
+// target : given sum value
+
+class Solution {
+  public:
+    int threeSumClosest(vector<int> arr, int target) {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        int closestSum = INT_MAX;
+        int minDiff = INT_MAX;
+
+        for (int i = 0; i < n - 2; ++i) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                int diff = abs(target - sum);
+
+                if (diff < minDiff || (diff == minDiff && sum > closestSum)) {
+                    closestSum = sum;
+                    minDiff = diff;
+                }
+
+                if (sum < target) {
+                    ++left;
+                } else if (sum > target) {
+                    --right;
+                } else {
+                    // If the sum is exactly equal to the target, return the sum.
+                    return sum;
+                }
+            }
+        }
+
+        return closestSum;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        // cin.ignore();
+        // if (f)
+        //     return 0;
+        Solution ob;
+        cout << ob.threeSumClosest(arr, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
