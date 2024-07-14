2ee65c6e7a3b2fd2857fdc4d9cc2f67c7a8ae3f6
Vishnu Burkhawala <145148109+Vishnu9804@users.noreply.github.com>
2024-07-14 15:30:03 +0530

    Create POTD_13_7_2024.cpp

---
+++ POTD_13_7_2024.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+        // Adjacency list representation of the graph
+        vector<vector<pair<int, int>>> adj(n + 1);
+        for (auto& edge : edges) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            adj[u].emplace_back(v, w);
+            adj[v].emplace_back(u, w);
+        }
+        
+        // Min-heap priority queue for Dijkstra's algorithm
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        
+        // Distance vector to store the minimum distance to each node from node 1
+        vector<int> dist(n + 1, INT_MAX);
+        // Parent array to reconstruct the path
+        vector<int> parent(n + 1, -1);
+        
+        // Initialize the source node
+        dist[1] = 0;
+        pq.emplace(0, 1); // (distance, node)
+        
+        while (!pq.empty()) {
+            int d = pq.top().first;
+            int u = pq.top().second;
+            pq.pop();
+            
+            if (d > dist[u]) continue;
+            
+            for (auto& neighbor : adj[u]) {
+                int v = neighbor.first;
+                int w = neighbor.second;
+                
+                if (dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    parent[v] = u;
+                    pq.emplace(dist[v], v);
+                }
+            }
+        }
+        
+        // If there's no path to node n
+        if (dist[n] == INT_MAX) {
+            return {-1};
+        }
+        
+        // Reconstruct the path
+        vector<int> path;
+        for (int v = n; v != -1; v = parent[v]) {
+            path.push_back(v);
+        }
+        reverse(path.begin(), path.end());
+        
+        // The first element is the weight of the path
+        path.insert(path.begin(), dist[n]);
+        
+        return path;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        map<pair<int, int>, int> mp;
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+            mp[{edges.back()[0], edges.back()[1]}] = edges.back()[2];
+            mp[{edges.back()[1], edges.back()[0]}] = edges.back()[2];
+        }
+
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        int ans = 0;
+        for (int i = 2; i < res.size(); i++) {
+            ans += mp[{res[i], res[i - 1]}];
+        }
+        if (ans == res[0]) {
+
+        } else if (res.size() == 1 && res[0] == -1)
+            ans = res[0];
+        else
+            ans = -2;
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
